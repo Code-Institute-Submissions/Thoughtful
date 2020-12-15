216ad2d4ea5ddd904e2dadd51fafc6821afb811d
TobyGreet <toby.greet@gmail.com>
2020-12-15 19:59:37 +0000

    function created for thought input form

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,3 +1,5 @@
+
+
 function treeFunction() {
    document.getElementsByTagName("body")[0].style.backgroundImage = "url(assets/images/tree.jpg)";
 }
@@ -8,4 +10,14 @@ function cityFunction() {
 
 function sunFunction() {
    document.getElementsByTagName("body")[0].style.backgroundImage = "url(assets/images/man.jpg)";
+}
+
+function addNewThought() {
+   let inputText = document.getElementById("thought-input").value;
+
+   let node = document.createElement("LI"); //<li></li> Create a <li> node
+   let textnode = document.createTextNode(inputText); // Create a text node
+   node.appendChild(textnode); // adding text from input to node <li> my text </li>
+   document.getElementById("thought").appendChild(node); 
+
 }
